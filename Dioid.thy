f61c24b31e9632359079453dd1e16f02e651527f
ElCondorSympa <martin92500@yahoo.fr>
2018-05-22 17:39:19 +0100

    Develop theory of topological dioid

--- Dioid.thy
+++ Dioid.thy
@@ -90,25 +90,26 @@ lemma (in abelian_cancel_monoid) is_abelian_monoid :
 subsection \<open>Canonically ordered monoids\<close>
 
 
-abbreviation
+definition
   canonic_order :: "('a, 'b) monoid_scheme => 'a => 'a \<Rightarrow> bool" (infixl "\<preceq>\<index>" 50)
-where "canonic_order G  \<equiv> factor G "
+where "canonic_order G x y  \<equiv> x = y \<or> (factor G x y \<and> (x \<in> carrier G) \<and> (y \<in> carrier G))"
 
 definition
   canonic_strict_order :: "('a, 'b) monoid_scheme => 'a => 'a \<Rightarrow> bool" (infixl "\<prec>\<index>" 50)
 where "canonic_strict_order G x y  = ((canonic_order G x y) \<and> \<not>(x = y))"
 
-abbreviation
+definition
   dioid_canonic_order :: "('a, 'b) ring_scheme => 'a => 'a \<Rightarrow> bool" (infixl "\<le>\<index>" 50)
-where "dioid_canonic_order R   \<equiv> factor (add_monoid R) "
+  where "dioid_canonic_order R x y \<equiv> x = y \<or>
+                                   (factor (add_monoid R) x y \<and> (x \<in> carrier R) \<and> (y \<in> carrier R))"
 
 definition
   dioid_canonic_strict_order :: "('a, 'b) ring_scheme => 'a => 'a \<Rightarrow> bool" (infixl "<\<index>" 50)
-where "dioid_canonic_strict_order R x y  = ((dioid_canonic_order R x y) \<and> \<not>(x = y))"
+where "dioid_canonic_strict_order R = (\<lambda> a b. a \<le>\<^bsub>R\<^esub> b \<and> a \<noteq> b)"
 
 lemma same_order :
 "dioid_canonic_order D = canonic_order (D\<lparr>mult := add D\<rparr>)"
-  unfolding  factor_def by auto
+  unfolding dioid_canonic_order_def canonic_order_def factor_def by auto
 
 abbreviation canonical_order ::  "_ \<Rightarrow> 'a gorder" where
   "canonical_order D \<equiv>
@@ -124,6 +125,7 @@ abbreviation dioid_canonical_order ::  "_ \<Rightarrow> 'a gorder" where
 
 lemma same_canonical_order : 
 "canonical_order (add_monoid D) = dioid_canonical_order D"
+  unfolding dioid_canonic_order_def canonic_order_def
    by simp
 
 locale canonical_order_monoid = comm_monoid +
@@ -134,13 +136,16 @@ lemma (in canonical_order_monoid) positive_condition :
 "\<lbrakk>a \<in> carrier G ; b \<in> carrier G ; a \<otimes> b = \<one> \<rbrakk> \<Longrightarrow> a = \<one> \<and> b = \<one> "
 proof
   fix a b assume aG : "a \<in> carrier G" and bG : "b \<in> carrier G" and abone : "a \<otimes> b = \<one>"
-  hence ainfone : "a \<preceq> \<one>" unfolding factor_def by force
+  hence ainfone : "a \<preceq> \<one>" unfolding factor_def dioid_canonic_order_def canonic_order_def by force
   moreover have "\<one> \<otimes> a = a" using l_one aG by auto
-  hence "\<one> \<preceq> a" unfolding factor_def using aG one_closed by auto
+  hence "\<one> \<preceq> a"
+    unfolding factor_def dioid_canonic_order_def canonic_order_def using aG one_closed by auto
   ultimately show "a = \<one>" using partial_order.le_antisym[OF canonically_ordered,of a \<one>] aG by auto
-  from aG bG abone have "b \<preceq> \<one>" unfolding factor_def using m_comm by force
+  from aG bG abone have "b \<preceq> \<one>"
+    unfolding factor_def dioid_canonic_order_def canonic_order_def using m_comm by force
   moreover have "\<one> \<otimes> b = b" using l_one bG by auto
-  hence "\<one> \<preceq> b" unfolding factor_def using bG one_closed by auto
+  hence "\<one> \<preceq> b"
+    unfolding dioid_canonic_order_def canonic_order_def factor_def using bG one_closed by auto
   ultimately show "b = \<one>" using partial_order.le_antisym[OF canonically_ordered,of b \<one>] bG by auto
 qed
 
@@ -156,18 +161,20 @@ proof
     obtain x where x_def : "x \<in> carrier G \<and> x \<notin> {\<one>}" using existx by blast
     hence "x \<noteq> \<one>" by blast
     moreover have "x \<preceq> \<one>"
-      unfolding factor_def
+      unfolding factor_def dioid_canonic_order_def canonic_order_def
       using one_closed group.inv_closed grpG group.r_inv_ex  grpG x_def by force
     moreover have "\<one> \<preceq> x"
-      unfolding factor_def using l_one x_def by force
+      unfolding factor_def dioid_canonic_order_def canonic_order_def
+      using l_one x_def by force
     ultimately have "x = \<one> \<and> x \<noteq> \<one>"
       using canonically_ordered
       unfolding partial_order_def weak_partial_order_def weak_partial_order_axioms_def
-      by (metis (full_types) eq_object.select_convs(1) gorder.select_convs(1)
-          one_closed partial_object.select_convs(1) x_def)
+                dioid_canonic_order_def canonic_order_def
+      apply simp.
     thus False by auto
   qed
 qed
+
 
 lemma (in canonical_order_monoid) submonoid_is_canonical_order :
   assumes "submonoid H G"
@@ -175,21 +182,24 @@ lemma (in canonical_order_monoid) submonoid_is_canonical_order :
 proof (intro canonical_order_monoid.intro)
   show "comm_monoid (G\<lparr>carrier := H\<rparr>)"    
     using comm_monoid.submonoid_is_comm_monoid canonical_order_monoid_axioms assms
-          canonical_order_monoid_def
+          canonical_order_monoid_def 
     by auto
   have "weak_partial_order (canonical_order (G\<lparr>carrier := H\<rparr>))"
     unfolding factor_def weak_partial_order_def equivalence_def weak_partial_order_axioms_def
+             canonic_order_def 
     apply simp apply auto
   proof-
-    have "\<And> x . x \<in> H \<Longrightarrow> \<one> \<in> H \<and> x \<otimes> \<one> = x"
-      using submonoid.one_closed[OF assms(1)]
-      by (meson assms r_one submonoid.mem_carrier)
-    thus "\<And>x. x \<in> H \<Longrightarrow> \<exists> z \<in> H . x = x \<otimes> z " by force
-    show "\<And>x c ca. c \<in> H \<Longrightarrow> ca \<in> H \<Longrightarrow> x = x \<otimes> c \<otimes> ca \<Longrightarrow> x \<in> H \<Longrightarrow> x \<otimes> c \<in> H \<Longrightarrow> x = x \<otimes> c"
-      using partial_order.le_antisym[OF canonically_ordered] assms factor_def
-      by (smt  gorder.select_convs(1) partial_object.select_convs(1) submonoid.mem_carrier)
-    show "\<And>x c ca. c \<in> H \<Longrightarrow> ca \<in> H \<Longrightarrow> x \<in> H \<Longrightarrow> x \<otimes> c \<in> H \<Longrightarrow> x \<otimes> c \<otimes> ca \<in> H \<Longrightarrow> \<exists>cb\<in>H.
-          x \<otimes> c \<otimes> ca = x \<otimes> cb"
+    have "\<And>x c ca. c \<in> carrier G \<Longrightarrow> x \<in> carrier G \<Longrightarrow> x \<otimes> c \<in> carrier G \<Longrightarrow> ca \<in> carrier G \<Longrightarrow>
+          x = x \<otimes> c \<otimes> ca \<Longrightarrow> x = x \<otimes> c"
+      using canonically_ordered
+      unfolding partial_order_def factor_def weak_partial_order_def weak_partial_order_axioms_def
+                dioid_canonic_order_def canonic_order_def
+      apply simp by (metis (no_types, lifting) m_closed)
+    thus "\<And>x c ca. c \<in> H \<Longrightarrow> x \<in> H \<Longrightarrow> x \<otimes> c \<in> H \<Longrightarrow> ca \<in> H \<Longrightarrow> x = x \<otimes> c \<otimes> ca \<Longrightarrow> x = x \<otimes> c"
+      using submonoid.mem_carrier assms
+      by smt
+    show "\<And>x c ca. c \<in> H \<Longrightarrow> x \<in> H \<Longrightarrow> x \<otimes> c \<in> H \<Longrightarrow> x \<otimes> c \<otimes> ca \<in> H \<Longrightarrow> ca \<in> H \<Longrightarrow>
+           \<forall>cb\<in>H. x \<otimes> c \<otimes> ca \<noteq> x \<otimes> cb \<Longrightarrow> x = x \<otimes> c \<otimes> ca"
       using canonical_order_monoid_axioms assms
       by (metis m_assoc submonoid.mem_carrier submonoid_def)
   qed
@@ -222,6 +232,13 @@ lemma canonical_order_monoid_hemi_groupI :
 locale abelian_hemi_group = abelian_cancel_monoid +
   assumes canonically_ordered :
        "partial_order (dioid_canonical_order D)"
+
+lemma (in abelian_hemi_group) is_hemi_group :
+"hemi_group (add_monoid D)"
+  using abelian_cancel_monoid_axioms canonically_ordered
+  unfolding hemi_group_def hemi_group_axioms_def abelian_cancel_monoid_def canonic_order_def
+        dioid_canonic_order_def
+  by auto
 
 subsection "Dioids"
 
@@ -267,25 +284,29 @@ lemma (in dioid) mult_preserves_order :
     and "a \<le>\<^bsub>D\<^esub> b"
   shows "(a \<otimes> c) \<le> (b \<otimes> c)"
 proof -
-  from assms obtain x where x_def : "x \<in> carrier D" " a \<oplus> x = b" by auto
+  from assms obtain x where x_def : "x \<in> carrier D" " a \<oplus> x = b"
+    unfolding factor_def dioid_canonic_order_def
+    by (metis monoid.select_convs(1) partial_object.select_convs(1) r_zero zero_closed)
   hence "(b \<otimes> c) = ((a \<oplus> x) \<otimes> c)" by simp
   also have "... = (a \<otimes> c) \<oplus>  (x \<otimes> c)"
     by (simp add: assms(1) assms(3) l_distr x_def(1))
   finally have "(b \<otimes> c) = (a \<otimes> c) \<oplus>  (x \<otimes> c)" by simp
-  thus "(a \<otimes> c) \<le> (b \<otimes> c)" using assms(3) x_def(1) by auto
+  thus "(a \<otimes> c) \<le> (b \<otimes> c)" 
+    unfolding dioid_canonic_order_def
+    using assms x_def(1) by auto
 qed
 
 lemma (in dioid) zero_is_idem :
 "\<zero> \<otimes> \<zero> = \<zero>"
 proof-
-  have "\<zero> \<le> \<zero> \<otimes> \<zero>" by auto
-  moreover have "\<zero> \<otimes> \<zero> \<le> \<zero> \<otimes> \<zero> \<otimes> \<zero>" by auto
+  have "\<zero> \<le> \<zero> \<otimes> \<zero>" unfolding   dioid_canonic_order_def by auto
+  moreover have "\<zero> \<otimes> \<zero> \<le> \<zero> \<otimes> \<zero> \<otimes> \<zero>" unfolding dioid_canonic_order_def by auto
   moreover have "\<zero> \<otimes> \<zero> \<otimes> \<zero>  \<le> \<zero>"
   proof-
     have "\<one> = (\<one> \<oplus> \<zero>) \<otimes> (\<one> \<oplus> \<zero>)" by simp
     also have "... = (\<one> \<oplus> \<zero>) \<otimes> \<one> \<oplus> (\<one> \<oplus> \<zero>) \<otimes> \<zero>" using semiring.l_distr by auto
     finally have "\<one> = \<one> \<oplus> \<zero> \<otimes> \<zero>" by simp
-    hence "\<zero> \<otimes> \<zero> \<le> \<one> " unfolding factor_def by auto
+    hence "\<zero> \<otimes> \<zero> \<le> \<one> " unfolding factor_def dioid_canonic_order_def by auto
     thus "\<zero> \<otimes> \<zero> \<otimes> \<zero>  \<le> \<zero>"
       using mult_preserves_order[where ?a = "\<zero> \<otimes> \<zero>" and ?b = "\<one>" and ?c = "\<zero>"] by auto
   qed
@@ -319,21 +340,27 @@ proof(intro dioid.intro semiring.intro abelian_hemi_group.intro)
     using semiring_axioms assms submonoid.mem_carrier submonoid_imp_subset
     by (smt l_distr l_null r_distr r_null subdioid_def)
   have "partial_order (canonical_order (add_monoid D\<lparr>carrier := H\<rparr>))"
-    using canonical_order_monoid.submonoid_is_canonical_order add.comm_monoid_axioms 
-    unfolding abelian_hemi_group_axioms_def canonical_order_monoid_def
-              canonical_order_monoid_axioms_def
-    by (metis same_canonical_order subdioid.sub_add assms canonically_ordered)
+    using canonical_order_monoid.submonoid_is_canonical_order add.comm_monoid_axioms assms
+          same_canonical_order[of D] subdioid.sub_add[OF assms]  canonically_ordered
+          hemi_group.is_canonical_order_monoid hemi_group.is_canonical_order_monoid
+          canonical_order_monoid.canonically_ordered is_hemi_group
+    by blast
   thus "abelian_hemi_group_axioms (D\<lparr>carrier := H\<rparr>)" unfolding abelian_hemi_group_axioms_def
     using same_canonical_order [where ?D = "D\<lparr>carrier := H\<rparr>"] by simp
 qed
 
+definition R :: "real ring"
+  where "R =(\<lparr>carrier = {r :: real . r \<ge> 0}, monoid.mult = op *, one = 1, zero = 0, add = op +\<rparr>)"
+
+
 lemma real_is_dioid :
-"dioid (\<lparr>carrier = {r :: real . r \<ge> 0}, monoid.mult = op *, one = 1, zero = 0, add = op +\<rparr>)"
+"dioid (R)" unfolding R_def
    apply unfold_locales 
-   unfolding factor_def
+   unfolding factor_def canonic_order_def dioid_canonic_order_def
    apply (simp_all add : distrib_right distrib_left)
    apply linarith
    by auto
+
 
 
 end
