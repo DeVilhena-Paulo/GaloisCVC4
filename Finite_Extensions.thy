bdcc66c340602b3f538a84fac74fa7585a8e3321
Paulo Emilio de Vilhena <pevilhena2@gmail.com>
2018-07-12 16:23:31 +0100

    Update Finite_Extensions.thy

--- Finite_Extensions.thy
+++ Finite_Extensions.thy
@@ -1,10 +1,9 @@
-(* ************************************************************************** *)
-(* Title:      Finite_Extensions.thy                                          *)
-(* Author:     Paulo Emílio de Vilhena                                        *)
-(* ************************************************************************** *)
+(*  Title:      HOL/Algebra/Finite_Extensions.thy
+    Author:     Paulo Emílio de Vilhena
+*)
 
 theory Finite_Extensions
-  imports Embedded_Algebras Polynomials RingHom
+  imports Embedded_Algebras More_Polynomials Polynomial_Divisibility
     
 begin
 
@@ -13,19 +12,17 @@ section \<open>Finite Extensions\<close>
 subsection \<open>Definitions\<close>
 
 definition (in ring) transcendental :: "'a set \<Rightarrow> 'a \<Rightarrow> bool"
-  where "transcendental K x \<longleftrightarrow> inj_on (\<lambda>p. eval p x) { p. polynomial R p \<and> set p \<subseteq> K }"
+  where "transcendental K x \<longleftrightarrow> inj_on (\<lambda>p. eval p x) (carrier (K[X]))"
 
-definition (in ring) algebraic :: "'a set \<Rightarrow> 'a \<Rightarrow> bool"
-  where "algebraic K x \<longleftrightarrow> \<not> transcendental K x"
+abbreviation (in ring) algebraic :: "'a set \<Rightarrow> 'a \<Rightarrow> bool"
+  where "algebraic K x \<equiv> \<not> transcendental K x"
 
-(*
 definition (in ring) Irr :: "'a set \<Rightarrow> 'a \<Rightarrow> 'a list"
-  where "Irr K x = (THE p. p \<noteq> [] \<and> pirreducible K p \<and> set p \<subseteq> K \<and> eval p x = \<zero> \<and> lead_coeff p = \<one>)"
-*)
+  where "Irr K x = (THE p. p \<in> carrier (K[X]) \<and> pirreducible K p \<and> eval p x = \<zero> \<and> lead_coeff p = \<one>)"
 
 inductive_set (in ring) simple_extension :: "'a set \<Rightarrow> 'a \<Rightarrow> 'a set"
   for K and x where
-    zero: "\<zero>\<^bsub>R\<^esub> \<in> simple_extension K x" |
+    zero: "\<zero> \<in> simple_extension K x" |
     lin:  "\<lbrakk> k1 \<in> K; k2 \<in> K \<rbrakk> \<Longrightarrow> (k1 \<otimes> x) \<oplus> k2 \<in> simple_extension K x"
 
 fun (in ring) finite_extension :: "'a set \<Rightarrow> 'a list \<Rightarrow> 'a set"
@@ -34,6 +31,49 @@ fun (in ring) finite_extension :: "'a set \<Rightarrow> 'a list \<Rightarrow> 'a
 
 subsection \<open>Basic Properties\<close>
 
+lemma (in ring) eval_transcendental:
+  assumes "(transcendental over K) x" "p \<in> carrier (K[X])" "eval p x = \<zero>" shows "p = []"
+proof -
+  have "[] \<in> carrier (K[X])" and "eval [] x = \<zero>"
+    by (auto simp add: univ_poly_def)
+  thus ?thesis
+    using assms unfolding over_def transcendental_def inj_on_def by auto
+qed
+
+lemma (in ring) transcendental_imp_trivial_ker:
+  shows "(transcendental over K) x \<Longrightarrow> a_kernel (K[X]) R (\<lambda>p. eval p x) = { [] }"
+  using eval_transcendental unfolding a_kernel_def' by (auto simp add: univ_poly_def)
+
+lemma (in ring) non_trivial_ker_imp_algebraic:
+  shows "a_kernel (K[X]) R (\<lambda>p. eval p x) \<noteq> { [] } \<Longrightarrow> (algebraic over K) x"
+  using transcendental_imp_trivial_ker unfolding over_def by auto
+
+lemma (in domain) trivial_ker_imp_transcendental:
+  assumes "subring K R" and "x \<in> carrier R"
+  shows "a_kernel (K[X]) R (\<lambda>p. eval p x) = { [] } \<Longrightarrow> (transcendental over K) x"
+  using ring_hom_ring.trivial_ker_imp_inj[OF eval_ring_hom[OF assms]]
+  unfolding transcendental_def over_def by (simp add: univ_poly_zero)
+
+lemma (in domain) algebraic_imp_non_trivial_ker:
+  assumes "subring K R" and "x \<in> carrier R"
+  shows "(algebraic over K) x \<Longrightarrow> a_kernel (K[X]) R (\<lambda>p. eval p x) \<noteq> { [] }"
+  using trivial_ker_imp_transcendental[OF assms] unfolding over_def by auto
+
+lemma (in domain) zero_is_algebraic:
+  assumes "subring K R" shows "(algebraic over K) \<zero>"
+proof -
+  have non_trivial_eval: "[ \<one>, \<zero> ] \<in> carrier (K[X])" "eval [ \<one>, \<zero> ] \<zero> = \<zero>"
+    by (auto simp add: univ_poly_def polynomial_def subringE(2-3)[OF assms])
+  show ?thesis
+    using eval_transcendental[OF _ non_trivial_eval] unfolding over_def by auto
+qed
+
+lemma (in domain) ker_diff_carrier:
+  assumes "subring K R" and "x \<in> carrier R"
+  shows "a_kernel (K[X]) R (\<lambda>p. eval p x) \<noteq> carrier (K[X])"
+proof (rule ccontr)
+
+(*
 lemma (in ring) transcendental_imp_trivial_ker:
   assumes "x \<in> carrier R"
   shows "(transcendental over K) x \<Longrightarrow> (\<And>p. \<lbrakk> polynomial R p; set p \<subseteq> K \<rbrakk> \<Longrightarrow> eval p x = \<zero> \<Longrightarrow> p = [])"
@@ -77,7 +117,7 @@ lemma (in domain) algebraic_iff:
   assumes "subring K R" "x \<in> carrier R"
   shows "(algebraic over K) x \<longleftrightarrow> (\<exists>p \<noteq> []. polynomial R p \<and> set p \<subseteq> K \<and> eval p x = \<zero>)"
   using non_trivial_ker_imp_algebraic[OF assms(2)] algebraic_imp_non_trivial_ker[OF assms] by auto
-
+*)
 
 (*
 proof -
